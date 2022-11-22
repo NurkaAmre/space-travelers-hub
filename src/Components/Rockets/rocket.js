cc10102fbd66296438755b645dda35769d5b1c8c
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-11-22 22:03:10 +0600

    Create a directory for all Redux state slice files

--- src/Components/Rockets/rocket.js
+++ src/Components/Rockets/rocket.js
@@ -1,8 +1,10 @@
 const Rocket = ({title, image, description, id}) => {
+
+    
     return (
         <div>
             <div>
-                <img src="{image}" alt="{title}" />
+                <img src={image} alt={title} />
                 <div>
                     <h2>{title}</h2>
                     <p>{description}</p>
