a862089125c665af9621eb0477771ec52fa85fe1
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-11-22 15:31:01 +0600

    Add header with empty navbar

---
+++ src/Components/navbar/Navbar.js
@@ -0,0 +1,25 @@
+const navBar = () => {
+    return (
+      <header>
+        <nav>
+          <div className="container">
+            {/* LOGO container*/}
+            <div className="logo">
+              {/* Logo link*/}
+              <img src="" alt="SpaceX Logo" />
+              {/* Logo text */}
+              <h3></h3>
+            </div>
+            {/* NavLinks List*/}
+            <ul role="list">
+              {/* NavLink items */}
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    )
+  }
+  
