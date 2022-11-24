d464a2006538eb68f10e6ab4e3ca1d94c0c0e954
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-11-25 00:38:42 +0600

    Add reserved rockets to Myprofile page

--- src/Components/MyProfile/Myprofile.js
+++ src/Components/MyProfile/Myprofile.js
@@ -1,54 +1,33 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { allRockets, rocketBooking } from '../redux/rockets/rocketsSlice';
+import { useSelector } from 'react-redux';
+import { allRockets } from '../redux/rockets/rocketsSlice';
+import './Myprofile.css';
 
 const UserProfile = () => {
-  const dispatch = useDispatch();
-
   const rockets = useSelector(allRockets);
 
   const rocketsReserved = rockets.filter((rocket) => rocket.reserved);
 
   return (
+    <div className="MyProfile">
+      <div className="MyProfileChild">
 
-    <div className="main">
-      <h2 className="myrocket">My Rockets</h2>
-      {rocketsReserved.length === 0 ? (
-        <>
-          <span className="italic">No Rockets Reserved, yet</span>
-          <span className="italic">Click on Rockets page to reserve.</span>
-        </>
-      ) : (
-        rocketsReserved.map(({ rocketId: id, rocketName, link }) => (
-          <div
-            key={id}
-            className="amre"
-          >
-            <h3>
-              {' '}
-              {rocketName}
-              <span>
-                <a
-                  className="link"
-                  href={link}
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  Read more
-                </a>
-              </span>
-            </h3>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="dark"
-              ripple={false}
-              onClick={() => dispatch(rocketBooking(id))}
-            >
-              Cancel
-            </button>
-          </div>
-        ))
-      )}
+        <h2 className="ChildTitle">My Rockets</h2>
+        {rocketsReserved.length === 0 ? (
+          <>
+            <span>No Rockets Reserved, yet</span>
+          </>
+        ) : (
+
+          rocketsReserved.map(({ rocketId: id, rocketName }) => (
+            <ul className="list">
+              <li key={id} className="listChild">
+                <h4>{rocketName}</h4>
+              </li>
+            </ul>
+          ))
+        )}
+      </div>
     </div>
 
   );
