a52f0dc0e684973b7794aaf084bfbab63c28144f
dadadon <davianberoni@yahoo.com>
2022-11-25 00:06:09 -0500

    Update files

--- src/Components/Missions/mission.js
+++ src/Components/Missions/mission.js
@@ -9,7 +9,7 @@ const Mission = (item) => {
       <th scope="row">{name}</th>
       <td>{desc}</td>
       <td className='align-self-center'><span className={status ? "badge bg-info text-dark" : "badge bg-secondary"}>{ status ? 'Active Member' : 'NOT A MEMBER' }</span></td>
-      <td><button type="button" className="btn btn-outline-dark"  onClick={() => handleClick(id,missionsL,status)}>Mission</button></td>
+      <td><button type="button" className={status ? "btn btn-outline-dark p-0": "btn btn-outline-danger p-0"}  onClick={() => handleClick(id,missionsL,status)}>{ status ? 'Leave Mission' : 'Join Mission' }</button></td>
       
     </tr>
   )
