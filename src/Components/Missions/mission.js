bac121cca02dcdc2551b9b09bf93415a3956fa63
dadadon <davianberoni@yahoo.com>
2022-11-24 23:57:09 -0500

    Add status and handleClick

--- src/Components/Missions/mission.js
+++ src/Components/Missions/mission.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const mission = (item) => {
-  const { name, desc, status } = item;
 
-  console.log(name);
+const Mission = (item) => {
+  const { id, name, desc, status, handleClick, missionsL } = item;
   return (
     <tr>
       <th scope="row">{name}</th>
       <td>{desc}</td>
-      <td className='align-self-center'><span class="badge bg-info text-dark">{status}</span></td>
-      <td><button type="button" class="btn btn-outline-dark">Mission</button></td>
+      <td className='align-self-center'><span className={status ? "badge bg-info text-dark" : "badge bg-secondary"}>{ status ? 'Active Member' : 'NOT A MEMBER' }</span></td>
+      <td><button type="button" className="btn btn-outline-dark"  onClick={() => handleClick(id,missionsL,status)}>Mission</button></td>
       
     </tr>
   )
 }
- export default mission;
+ export default Mission;
