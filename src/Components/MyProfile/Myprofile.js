0621c9fb28253eba30696558333ded1f61ed8297
dadadon <davianberoni@yahoo.com>
2022-11-25 11:30:08 -0500

    Add missions to my profile

--- src/Components/MyProfile/Myprofile.js
+++ src/Components/MyProfile/Myprofile.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { allRockets } from '../redux/rockets/rocketsSlice';
-import './Myprofile.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import { allRockets } from "../redux/rockets/rocketsSlice";
+import "./Myprofile.css";
 
 const UserProfile = () => {
   const rockets = useSelector(allRockets);
-
+  const missionItems = useSelector((state) => state.mission);
+  const reservedMissions = missionItems.filter((missionItems) => missionItems.status);
   const rocketsReserved = rockets.filter((rocket) => rocket.reserved);
 
   return (
     <div className="MyProfile">
       <div className="MyProfileChild">
-
         <h2 className="ChildTitle">My Rockets</h2>
         {rocketsReserved.length === 0 ? (
           <>
             <span>No Rockets Reserved, yet</span>
           </>
         ) : (
-
           rocketsReserved.map(({ rocketId: id, rocketName }) => (
             <ul key={id} className="list">
               <li key={id} className="listChild">
@@ -27,9 +26,21 @@ const UserProfile = () => {
             </ul>
           ))
         )}
+        {reservedMissions.length === 0 ? (
+          <>
+            <span>No Rockets Reserved, yet</span>
+          </>
+        ) : (
+          reservedMissions.map(({ missionId: id, mission_name }) => (
+            <ul key={id} className="list">
+              <li key={id} className="listChild">
+                <h4>{mission_name}</h4>
+              </li>
+            </ul>
+          ))
+        )}
       </div>
     </div>
-
   );
 };
 
